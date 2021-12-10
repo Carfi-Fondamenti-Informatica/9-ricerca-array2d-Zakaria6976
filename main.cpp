0d6e0fa835992da3fd50fa34401b2ee0ad232259
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-12-10 21:27:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+
+using namaspace std;
+
 
 int main(){
+  int dim;
+  cin>> dim;
+  float numeri[dim];
+  for(int i=0;i<dim;i++){
+    cin>> numeri[i];
+  }
+  numeroMassimo(numeri,dim);
+  cout << numeroMassimo(numeri , dim) <<endl;
   return 0;
 }
