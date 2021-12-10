c3aa87864969e5b2f74c1795157928e77de2f7e4
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-12-10 21:41:41 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+#include <cstring>
 
+bool ricerca(char lista [10] [20],char nome[1][20],int &pos){
+  
+  for(int =0;i<10;i++){
+    if(strcmp(lista[i],nome [0])==0){
+      pos = i;
+      return true;
+    }
+  }
+  return false;
+}
